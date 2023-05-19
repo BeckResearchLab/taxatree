5333655e5f7bc1f50e076ffe64ffaac9504dc7d0
Dave Beck <dacb@uw.edu>
2023-05-18 21:33:04 -0700

    simple csv to fasta with taxid

--- csvtofasta.py
+++ csvtofasta.py
@@ -10,6 +10,15 @@ import pandas as pd
 
 # the input csv has column 0 as a numbered index, we want taxonid
 # use the rest of the colums
-csv = pd.read_csv("taxa.csv", index_col=1, usecols=range(2,11))
+csv = pd.read_csv("taxa.csv", usecols=range(1,11))
+csv.set_index("taxid", inplace=True)
 
-print(csv.head())
+print(csv.head(2))
+
+seqfield = "16s_seq"
+# note: use the trailing underscore below for parsing downstream
+# files with sed or other string matching to prevent partial 
+# matches from replacing, e.g. 1234 matching 12345
+# useful for subbing in full tax or other info
+for index, row in csv.iterrows():
+    print(f">taxid_{index}_\n{row[seqfield]}")
